81ad63f3368ff6b8cdd6ba7ee69e0cdb9f81613e
Nguyen Do Nhat Tan <personatan25@gmail.com>
2024-08-19 22:28:46 +0700

    Search bar UI done

--- src/app/Components/Main/app.component.ts
+++ src/app/Components/Main/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {NavBarComponent} from "../nav-bar/nav-bar.component";
+import {HomePageComponent} from "../home-page/home-page.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavBarComponent],
+  imports: [RouterOutlet, NavBarComponent, HomePageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 
