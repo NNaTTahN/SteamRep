222438eccc6ffbc65dec844e7a6791380e5ab9a1
Nguyen Do Nhat Tan <personatan25@gmail.com>
2024-08-19 22:29:54 +0700

    fix some shit so i look good when i scale shit down

---
+++ src/app/home-nav/home-nav.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-nav.component.html',
+  styleUrl: './home-nav.component.css'
+})
+export class HomeNavComponent {
+
+}
