e2936e11e6b71a582f59d7abf8d63105782d54a2
Nguyen Do Nhat Tan <personatan25@gmail.com>
2024-08-22 05:12:08 +0700

    Home page game list WIP

--- src/app/Components/home-page/home-page.component.ts
+++ src/app/Components/home-page/home-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule} from "@angular/common";
+<<<<<<< HEAD
 import { GameListLayoutComponent} from "./game-list-layout/game-list-layout.component";
 import { SlideItemComponent} from "./Slide Item/slideItem.component";
 import {HomeSliderComponent} from "./home-slider/home-slider.component";
+=======
+>>>>>>> a8eced8 (Home page game list WIP)
 
 @Component({
   selector: 'app-home-page',
@@ -12,6 +15,7 @@ import {HomeSliderComponent} from "./home-slider/home-slider.component";
   styleUrl: './home-page.component.css'
 })
 export class HomePageComponent {
+  activeButton: string | null = 'newAndTrending';
 
   buttonClicked(button: string) {
     this.activeButton = button;
